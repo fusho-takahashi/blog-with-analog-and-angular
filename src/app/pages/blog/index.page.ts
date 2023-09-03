67047c87fa5620e9cd93d9eff6c0bdc03bbf2732
fusho-takahashi <zenryaku@fusho-takahashi.me>
2023-09-03 18:54:13 +0900

    Build a Blog with Analog and Angular

---
+++ src/app/pages/blog/index.page.ts
@@ -0,0 +1,22 @@
+import { injectContentFiles } from '@analogjs/content';
+import { AsyncPipe, NgFor } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { BlogPost } from '../../models/post';
+
+@Component({
+  standalone: true,
+  imports: [NgFor, RouterLink, AsyncPipe],
+  template: `
+    <h2>Posts</h2>
+
+    <ul>
+      <li *ngFor="let post of posts">
+        <a [routerLink]="['/blog', post.slug]">{{ post.attributes.title }}</a>
+      </li>
+    </ul>
+  `,
+})
+export default class IndexPage {
+  posts = injectContentFiles<BlogPost>();
+}
